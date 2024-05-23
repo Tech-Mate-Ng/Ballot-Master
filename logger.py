30ca8686f4ee9c78aac65782b99bb758f1d45b46
Ukeme Edet <ukemeedet2207@gmail.com>
2024-05-23 15:38:10 +0100

    chore: Configure logging settings

---
+++ logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+This module provides functions for configuring the logging system.
+"""
+import logging
+
+
+def configure_logging():
+    """
+    Configures the logging settings.
+
+    This function sets up the logging module with the specified settings.
+    It configures the log file path, log level, and log message format.
+
+    Args:
+            None
+
+    Returns:
+            None
+    """
+    logging.basicConfig(
+        filename="/home/ukeme/Desktop/bp/log.txt",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
