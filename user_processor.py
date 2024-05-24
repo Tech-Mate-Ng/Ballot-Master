92ae7bbd194fc699d09a79c991fc2bcf14007235
Ukeme Edet <ukemeedet2207@gmail.com>
2024-05-24 15:38:42 +0100

    chore: Add user accommodation reservation processing functionality

---
+++ user_processor.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+This script processes user data and makes accommodation reservations for eligible users.
+"""
+
+import time
+import requests
+import json
+import logging
+from credentials import get_credentials
+
+
+def process_user(i):
+    """
+    Process a user's accommodation reservation.
+
+    Args:
+            i (int): The user's identifier.
+
+    Returns:
+            None
+
+    Raises:
+            None
+    """
+    username, password, hall, day = get_credentials(i)
+    if day != time.strftime("%d"):
+        logging.info(f"Skipping user {i} as it is not their day")
+        logging.info(f"Day: {day} Current Day: {time.strftime('%d')}")
+        return
+
+    base_url = "http://studentportalbeta.unilag.edu.ng/"
+    hall_id_dict = {
+        "Mariere Hall": "MARIERE%20HALL",
+        "Moremi Hall": "MOREMI%20HALL",
+        "Fagunwa Hall": "FAGUNWA%20HALL",
+        "Jaja Hall": "JAJA%20HALL",
+        "Kofo Hall": "KOFO%20HALL",
+        "Makama Hall": "MAKAMA%20HALL",
+        "Sodeinde Hall": "SODEINDE%20HALL",
+        "Eni Njoku Hall": "ENI%20NJOKU%20HALL",
+    }
+
+    with requests.Session() as s:
+        response = s.post(
+            f"{base_url}users/login",
+            data={"MatricNo": username, "Password": password},
+        )
+        if response.status_code != 200:
+            logging.error(f"Failed to login user {i}")
+            return
+        else:
+            logging.info(
+                f"Successfully logged in {json.loads(response.text)['Data']['Student']['FullName']}"
+            )
+
+        s.cookies["_auth"] = response.text
+        general_header = {
+            "Authorization": f"Bearer {json.loads(response.text)['Data']['Token']}",
+            "Content-Type": "application/json",
+            "Host": "studentportalbeta.unilag.edu.ng",
+            "Referer": "http://studentportal.unilag.edu.ng/",
+        }
+
+        response = s.get(
+            f"{base_url}accomodation/accomodationHalls",
+            headers=general_header,
+        )
+        logging.info(f"Response from accomodationHalls: {response.text}")
+
+        res = json.dumps({"HallId": hall_id_dict[hall]})
+        response = s.post(
+            f"{base_url}accomodation/saveAccommodationReservation",
+            data=res,
+            headers=general_header,
+        )
+        logging.info(
+            f"Response from saveAccommodationReservation: {response.text}"
+        )
